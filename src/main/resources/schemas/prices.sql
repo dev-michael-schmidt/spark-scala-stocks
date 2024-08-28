8d7a31e6133c52954bb47cf39a605b17a058bd3a
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-08-27 21:50:43 -0500

    Create prices table SQL

---
+++ src/main/resources/schemas/prices.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prices (
+    id INTEGER PRIMARY KEY NOT NULL,
+    event_ts INT NOT NULL,
+    symbol VARCHAR(8) NOT NULL,
+    open DOUBLE PRECISION,
+    high DOUBLE PRECISION,
+    low DOUBLE PRECISION,
+    close DOUBLE PRECISION,
+    adj_close DOUBLE PRECISION,
+    volume INT  -- See TODO` on spark DataFrame schema.  Currently DoubleType
+);
