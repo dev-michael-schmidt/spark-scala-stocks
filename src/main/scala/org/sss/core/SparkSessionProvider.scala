10fbe4ac44cbd5ac6c1d12156cf881b01eecfe0e
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-08-30 16:08:56 -0500

    Commented in config details for running on K8s

--- src/main/scala/org/sss/core/SparkSessionProvider.scala
+++ src/main/scala/org/sss/core/SparkSessionProvider.scala
@@ -4,14 +4,18 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSessionProvider {
 
-  private val configs = Map(
-    "spark.master" -> "local",
-    "spark.executor.memory" -> "6g",
-    "spark.executor.cores" -> "4"
-  )
-
+private val configs = Map(
+  "spark.master" -> "local"
+)
+  /*
+  "spark.master" -> "k8s://https://<your-k8s-api-server>:<port>",   // configuration for Kubernetes
+  "spark.kubernetes.container.image" -> "<your-spark-image>",       // Docker image for Spark
+  "spark.kubernetes.namespace" -> "<your-namespace>",               // Kubernetes namespace
+  "spark.kubernetes.authenticate.driver.serviceAccountName" -> "<your-service-account>",  // Service account for driver
+  "spark.executor.instances" -> "2",                                // Number of executors
+  "spark.kubernetes.driver.pod.name" -> "<your-driver-pod-name>"    // Name of the driver pod
+*/
   private val sparkBuilder = SparkSession.builder()
-    .config("spark.master", "local")
     .appName(System.getenv("APP_NAME"))
 
    configs.foreach { case (key, value) => sparkBuilder.config(key, value) }
