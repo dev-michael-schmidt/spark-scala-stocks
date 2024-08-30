497b1d708fa019c41dc0124595f180d5500e2fef
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-08-30 16:08:32 -0500

    Simpy python script testing connection and failure to postgres using psycopg2

---
+++ debug/postgres_debug.py
@@ -0,0 +1,39 @@
+import psycopg2
+
+# Connection parameters
+db_host = "postgres"
+db_port = "5432"
+db_name = "stocks"
+db_user = "airflow"
+db_password = "airflow"
+
+connection = psycopg2.connect(
+    host=db_host,
+    port=db_port,
+    database=db_name,
+    user=db_user,
+    password=db_password
+)
+
+# Create a cursor object using the connection
+cursor = connection.cursor()
+
+
+print('')
+print('')
+print('')
+# should pass
+cursor.execute("SELECT * FROM prices;")
+print('should have passed')
+print('')
+print('')
+print('')
+
+print('connection had to happen')
+
+cursor.execute("SELECT * FROM public.priceeouas;")
+print('should have failed')
+
+# Fetch the result of the query
+# db_version = cursor.fetchone()
+# print(f"Connected to PostgreSQL database, version: {db_version}")
