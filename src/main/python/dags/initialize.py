7db48f94649c4c3002ca2822abedc2dc3a095a02
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-08-30 16:05:56 -0500

    change Int's to String's.  Probably doesn't matter anyway

--- src/main/python/dags/initialize.py
+++ src/main/python/dags/initialize.py
@@ -5,8 +5,8 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 
 env_vars = {
     'APP_NAME': 'SparkScalaStocks',
-    'P1': 1719842400,
-    'P2': 1722520800,
+    'P1': "1719842400",
+    'P2': "1722520800",
     'INTERVAL': '1d',
     'SYMBOL': 'SPY',
     'EVENTS': 'history',
@@ -18,11 +18,12 @@ env_vars = {
     'DB_DRIVER': 'org.postgresql.Driver',
 }
 
-with DAG(dag_id="hello_world_dag",
+with DAG(dag_id="hello_world_dag2",
          start_date=datetime(2024, 6, 1),
          schedule_interval="@hourly",
          catchup=False) as dag:
     initialize = SparkSubmitOperator(task_id="BAAAAAZZZZ",
-                                     application='/opt/airflow/libs/sss-0.1-bar.jar',
+                                     application='/opt/airflow/lib/load_symbol/sss-0.1.jar',
                                      env_vars=env_vars
                                      )
+    initialize
