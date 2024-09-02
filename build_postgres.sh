b58f33ed6a33ca78e2aa85cc9ddd9c2d34ee1d25
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-09-02 18:02:59 -0500

    add scratch commands

--- build_postgres.sh
+++ build_postgres.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-#docker exec -it sss_postgres psql -U postgres -c "CREATE DATABASE stocks;"
+
+# This appears to not be needed
+#docker exec -it sss_postgres psql -U airflow -d stocks -c "CREATE USER airflow SUPERUSER PASSWORD 'airflow';"
+
+# these appear to be needed.  possibly creating the DB and user make the airflow init DB easier
+docker exec -it sss_postgres psql -U airflow -d stocks -c "GRANT ALL PRIVILEGES ON DATABASE stocks TO airflow;"
+docker exec -id sss_postgres psql -U airflow postgres -c "CREATE DATABASE airflow;"
+# in container: CREATE USER airflow
+GRANT ALL ON SCHEMA public TO airflow;
+
+#docker exec -it sss_postgres psql -U airflow -c "CREATE DATABASE stocks;"
 # user airflow seems to exist
 #docker exec -it sss_postgres psql -U airflow -d stocks -c "CREATE USER airflow SUPERUSER PASSWORD 'airflow';"
-#docker exec -it sss_postgres psql -U airflow -d stocks -c "GRANT ALL PRIVILEGES ON DATABASE stocks TO airflow;"
 # use schema to create a table (can you "pipe" the schema: like -c "{piped_schema_file(s)}"
 
 echo "==================     Help for psql   ========================="
