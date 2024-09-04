c8b351b6fdd0b9e107accbc3ba9ac498b9e5b3dc
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-09-03 22:20:16 -0500

    Update TODO's

--- src/main/scala/org/sss/core/EndpointDataLoader.scala
+++ src/main/scala/org/sss/core/EndpointDataLoader.scala
@@ -13,20 +13,25 @@ object EndpointDataLoader {
 
   private val spark = SparkSessionProvider.getSparkSession
 
-  private val period1 = System.getenv("P1")
-  private val period2 = System.getenv("P2")
+  private val period1 = 915170400 // System.getenv("P1")
+  private val period2 = 1704088800 // System.getenv("P2")
   private val interval = System.getenv("INTERVAL")
   private val symbol = System.getenv("SYMBOL")
   private val events = System.getenv("EVENTS")
 
   private val driver = System.getenv("DB_DRIVER")
   private val user = System.getenv("POSTGRES_USER")
-  private val password = System.getenv("POSTGRES_PASSWORD")
+  private val password = System.getenv("POSTGRES_PASS")
+
+  // TODO: Okay for dev, use Secrets manager in prod
+  private val password = "airflow" // System.getenv("POSTGRES_PASSWORD") // don't use env's in prod either
+
   private val database = System.getenv("POSTGRES_DB")
   private val mode = System.getenv("DB_SAVE_MODE")   //! currently overwrite
   private val schema = Common.yahooAPISchema
 
-  private val url = s"jdbc:postgresql://localhost:5432/$database" // TODO parameterize this?
+  // TODO: use a env-var for postgres host
+  private val url = s"jdbc:postgresql://sss-postgres:5432/$database"
 
   def fromAPI(): DataFrame = {
     val client = HttpClient.newHttpClient()
@@ -49,6 +54,8 @@ object EndpointDataLoader {
       val values = rows.tail.map(BigDecimal(_).setScale(4, RoundingMode.HALF_UP).toDouble)
       Row.fromSeq(date +: values)
     }
+    // TODO: logging
+    println(s"######################## Got return code: ${response.statusCode()}")
 
     val dataFrame = spark.createDataFrame(spark.sparkContext.parallelize(rowData), schema)
       .withColumn("symbol", lit(symbol))
@@ -61,20 +68,27 @@ object EndpointDataLoader {
       .withColumn("event_ts", unix_timestamp(col("Date"), "yyyy-MM-dd").cast(LongType))
       .drop("Date")
 
+    println("########################  We've made the dataFrame")
+
     dataFrame
   }
   //noinspection AccessorLikeMethodIsUnit
   def toDatabase(dataFrame: DataFrame, table: String): Unit = {
 
+    dataFrame.show(3)
+
+    println("########################  Attempting to write")
     dataFrame.write
-      .format("jdbc")
-      .option("driver", driver)
-      .option("url", url)
-      .option("user", user)
-      .option("password", password)
-      .option("dbtable", table)
-      .mode(mode) // currently overwrite (or see val above)
-      .save()
+    .format("jdbc")
+    .option("url", url)
+    .option("dbtable", table)
+    .option("user", user)
+    .option("password", password)
+    .option("driver", driver)
+    .mode(mode)
+    .save()
+
+    println("@@@@@@@@@@@@@@@@@@@@ Write COMPLETED @@@@@@@@@@@@@@@@@@@@")
   }
 
   def fromDatabase(table: String): DataFrame = {
@@ -84,7 +98,7 @@ object EndpointDataLoader {
       .option("driver", driver)
       .option("url", url)
       .option("user", user)
-      .option("password", password)
+      .option("password", "airflow") // in local, dev environment, this is not a concern TODO: secret's manager
       .option("dbtable", table)
       .load()
 
