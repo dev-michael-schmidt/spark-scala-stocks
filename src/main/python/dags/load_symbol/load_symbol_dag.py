9ae840988ed4afef42e53349649ae0b267b3ecdd
Michael Schmidt <Michael.W.Schmidt@proton.me>
2024-09-08 23:16:25 -0500

    disable DockerOperator, prices -> tables

--- src/main/python/dags/load_symbol/load_symbol_dag.py
+++ src/main/python/dags/load_symbol/load_symbol_dag.py
@@ -14,7 +14,7 @@ env_vars = {
     'POSTGRES_USER': 'airflow',
     'POSTGRES_PASSWORD': 'airflow',  # TODO: Okay for dev, use secrets in prod
     'POSTGRES_DB': 'airflow',
-    'PRICES': 'prices',
+    'TABLE': 'prices',
     'DB_SAVE_MODE': 'overwrite',
     'DB_DRIVER': 'org.postgresql.Driver',
     'HADOOP_CONF_DIR': 'not-used',
@@ -26,17 +26,17 @@ with DAG(dag_id="load_symbol",
          schedule_interval="@hourly",
          catchup=False) as dag:
     load_spark_submit_spy = SparkSubmitOperator(task_id="spark_submit_spy",
-                                                application='/opt/airflow/dags/jars/load_spy-1.0.jar',
+                                                application='/opt/airflow/jars/load_symbol/spark-scala-stocks-1.0.jar',
                                                 env_vars=env_vars,
                                                 )
 
     # docker build -t sss-ls-1.0 .
-    load_docker_spy = DockerOperator(task_id='docker_load_spy',
-                                 image='sss-ls-1.0',
-                                 command='java -jar sss-ls-1.0.jar',
-                                 container_name='sss-docker-load',
-                                 environment=env_vars,
-                                 network_mode='container:spark-scala-stocks_default',
-                                 auto_remove='success')
+    # load_docker_spy = DockerOperator(task_id='docker_load_spy',
+    #                              image='sss-ls-1.0',
+    #                              command='java -jar sss-ls-1.0.jar',
+    #                              container_name='sss-docker-load',
+    #                              environment=env_vars,
+    #                              network_mode='container:spark-scala-stocks_default',
+    #                              auto_remove='success')
 
-    load_spark_submit_spy >> load_docker_spy
+    load_spark_submit_spy #  >> load_docker_spy
