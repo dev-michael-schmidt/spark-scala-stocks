54ad33f59e3aa5b3bbc6a1449c783f775957e6f6
Michael Schmidt <michael.w.schmidt@proton.me>
2024-09-10 11:02:23 -0500

    rename column to prevent data type errers

--- src/main/resources/schemas/prices.sql
+++ src/main/resources/schemas/prices.sql
@@ -1,11 +1,11 @@
 CREATE TABLE prices (
     id SERIAL PRIMARY KEY NOT NULL,
-    event_ts INT NOT NULL,
+    tstamp INT NOT NULL,
     symbol VARCHAR(8) NOT NULL,
     open DOUBLE PRECISION,
     high DOUBLE PRECISION,
     low DOUBLE PRECISION,
     close DOUBLE PRECISION,
     adj_close DOUBLE PRECISION,
-    volume INT  -- See TODO` on spark DataFrame schema.  Here Int, There DoubleType
+    volume INT
 );
