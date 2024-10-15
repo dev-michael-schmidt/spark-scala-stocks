fe078aa4eecfe23c2ce0d4da44b8cd098b76eef3
Michael Schmidt <michael.w.schmidt@proton.me>
2024-10-15 14:21:15 -0500

    "we're reached the end"

--- src/main/scala/org/sss/core/LoadSymbol.scala
+++ src/main/scala/org/sss/core/LoadSymbol.scala
@@ -1,15 +1,16 @@
 package org.sss.core
 
-import org.sss.core.EndpointDataLoader.{fromV8API, toDatabase}
+import org.sss.core.EndpointDataLoader._
 
 object LoadSymbol {
 
-  private val table = System.getenv("PRICES")
+  private val table = System.getenv("SYMBOL")
 
   def main(args: Array[String]): Unit = {
 
     val symbolDataFrame = fromV8API()
     toDatabase(symbolDataFrame, table)
 
+    println("we've reached the end")
   }
 }
