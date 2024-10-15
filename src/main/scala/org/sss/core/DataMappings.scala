3c5dce36b2e0d996d6b423bf2dce4d8466853e44
Michael Schmidt <michael.w.schmidt@proton.me>
2024-10-15 14:15:33 -0500

    Explicitly display param.

--- src/main/scala/org/sss/core/DataMappings.scala
+++ src/main/scala/org/sss/core/DataMappings.scala
@@ -1,9 +1,5 @@
 package org.sss.core
 
-//import io.circe._
-//import io.circe.generic.auto._
-//import io.circe.parser._
-
 import org.apache.spark.sql.types.{DoubleType, LongType, StructField, StructType}
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
@@ -11,12 +7,13 @@ import org.json4s.jackson.JsonMethods._
 
 object DataMappings extends App {
 
-  val validRanges: List[String] = List("1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max")
+  private val validRanges: List[String] = List("1d", "5d", "1mo", "3mo", "6mo", "1y", "2y", "5y", "10y", "ytd", "max")
+  private val sixtyDaySpan: Long = 5184000
+  private val sevenDaySpan: Long =604800
 
-  // case class Timestamp(timestamps: List[Long])
+  case class Timestamp(timestamps: List[Long])
   case class TradingPeriod(timezone: String, end: Long, start: Long, gmtoffset: Int)
   case class CurrentTradingPeriod(pre: TradingPeriod, regular: TradingPeriod, post: TradingPeriod)
-
   case class Meta(currency: String,
                   symbol: String,
                   exchangeName: String,
@@ -63,11 +60,57 @@ object DataMappings extends App {
   }
 
   def makeV7Url(symbol: String, period1: Long, period2: Long, interval: String, events: String): String = {
-    "https://query1.finance.yahoo.com/v7/finance/download/" + urlParameters
+    "https://query1.finance.yahoo.com/v7/finance/download/" + urlParameters(
+      sym = symbol,
+      p1 = period1,
+      p2 = period2,
+      ivl = interval,
+      e = events)
+
   }
 
   def makeV8Url(symbol: String, period1: Long, period2: Long, interval: String, events: String): String = {
-    "https://query2.finance.yahoo.com/v8/finance/chart/" + urlParameters(symbol, period1, period2, interval, events)
+    "https://query2.finance.yahoo.com/v8/finance/chart/" + urlParameters(
+      sym = symbol,
+      p1 = period1,
+      p2 = period2,
+      ivl = interval,
+      e = events)
+  }
+
+  def validateTimescales(period1: Long, period2: Long, interval: String): Boolean = {
+
+    if (period1 <= 0 || period2 <= 0) {
+      false
+    }
+
+    val now: Long = System.currentTimeMillis / 1000
+    val span: Long = period2 - period1
+
+    true
+  }
+
+    /*
+    minutes
+    1m - only 7 days worth
+    2m - the requested range must be within the last 60 days
+    5m - the requested range must be within the last 60 days
+    15m - the requested range must be within the last 60 days
+    30m - the requested range must be within the last 60 days
+    90m - the requested range must be within the last 60 days
+    1h - within the last 730 days
+
+    1d - No known limit
+    5d - No known limit
+
+    1wk - No known limit
+
+    1mo - No known limit
+    3mo - No known limit
+
+
+
+     */
   }
 }
 
