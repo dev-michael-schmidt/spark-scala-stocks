d7ea47e252bb0650af99b8609e1e55c5cc11ee07
Michael Schmidt <michael.w.schmidt@proton.me>
2024-10-15 14:20:44 -0500

    Add data rounding following exchange precision

--- src/main/scala/org/sss/core/EndpointDataLoader.scala
+++ src/main/scala/org/sss/core/EndpointDataLoader.scala
@@ -1,7 +1,7 @@
 package org.sss.core
 
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.LongType
+import org.apache.spark.sql.types.{LongType, MetadataBuilder}
 import org.apache.spark.sql.{DataFrame, Row}
 import org.json4s.native.JsonMethods._
 import org.json4s.{DefaultFormats, _}
@@ -18,21 +18,22 @@ object EndpointDataLoader {
   implicit val formats: DefaultFormats.type = DefaultFormats   // Required for extracting values (json4s)
 
   /* API */
-  private val period1 = System.getenv("P1").toInt
-  private val period2 = System.getenv("P2").toInt
+  private val period1 = System.getenv("P1").toLong
+  private val period2: Long = System.currentTimeMillis / 1000
+  // System.getenv("P2").toInt
   private val interval = System.getenv("INTERVAL")
   private val symbol = System.getenv("SYMBOL")
   private val events = System.getenv("EVENTS")
 
   /* Postgres */
-  private val p_host = System.getenv("POSTGRES_HOST")
+  private val p_host = "localhost" //System.getenv("POSTGRES_HOST")
   private val p_port = System.getenv("POSTGRES_PORT")
   private val user = System.getenv("POSTGRES_USER")
   private val password = "airflow" // System.getenv("POSTGRES_PASSWORD") // don't use env's in prod either
   private val driver = System.getenv("DB_DRIVER")
   private val database = System.getenv("POSTGRES_DB")
   private val mode = System.getenv("DB_SAVE_MODE")   //! currently overwrite
-  private val dbUrl = s"jdbc:postgresql://${p_host}/${p_port}$database"
+  private val dbUrl = s"jdbc:postgresql://${p_host}:${p_port}/$database"
 
   private val schema = DataMappings.getYahooAPISchema
   private val financeUrl = DataMappings.makeV8Url(symbol = symbol,
@@ -40,6 +41,36 @@ object EndpointDataLoader {
     period2 = period2,
     interval = interval,
     events = events)
+
+  private val roundPrecision = 4
+
+  def roundAt(precision: Int)(n: Double): Double = { val s = math pow (10, precision); (math round n * s) / s }
+
+  def flattenJson(json: JObject, prefix: String = ""): Map[String, Any] = {
+    json.obj.flatMap {
+      case (key, JString(value)) =>
+        Map(s"$prefix$key" -> value)
+      case (key, JInt(value)) =>
+        Map(s"$prefix$key" -> value)
+      case (key, JBool(value)) =>
+        Map(s"$prefix$key" -> value)
+      case (key, JDouble(value)) =>
+        Map(s"$prefix$key" -> value)
+
+      // Recursive call to handle nested objects.
+      // Note: the recursive calls need to happen *last* for Tail-end recursion
+      case (key, JObject(fields)) =>
+        flattenJson(JObject(fields), s"$prefix$key.") // recursive call
+      case (key, JArray(values)) =>
+        values.zipWithIndex.flatMap {
+          case (nestedValue, idx) =>
+            flattenJson(JObject(key -> nestedValue), s"$prefix$key[$idx].") // Recursive call
+        }.toMap
+      case (key, _) =>
+        // Handle other cases as needed
+        Map.empty[String, Any]
+    }.toMap
+  }
 
   def fromV8API(): DataFrame = {
     val client = HttpClient.newHttpClient()
@@ -52,18 +83,33 @@ object EndpointDataLoader {
     val json = parse(response.body())
 
     val chart = (json \ "chart").asInstanceOf[JObject]
-    val results = (chart \ "result").asInstanceOf[JArray]
+    val result = (chart \ "result").asInstanceOf[JArray]
+    val metadataJObject = (result \ "meta")
+      .asInstanceOf[JArray]
+      .arr
+      .head
+      .asInstanceOf[JObject]
+
+    val metaData = flattenJson(metadataJObject)
     val quotes = for {
-      JObject(resultItem) <- results.arr
-      JField("indicators", JObject(indicators)) <- resultItem
+      JObject(resultItem) <- result.arr
       JField("timestamp", JArray(timestamp)) <- resultItem
+      JField("indicators", JObject(indicators)) <- resultItem
       JField("quote", JArray(quoteItems)) <- indicators
     } yield {
       quoteItems.map { quote =>
-        val high = (quote \ "high").extract[List[Double]]
-        val low = (quote \ "low").extract[List[Double]]
-        val open = (quote \ "open").extract[List[Double]]
-        val close = (quote \ "close").extract[List[Double]]
+        val high = (quote \ "high")
+          .extract[List[Double]]
+          .map(n => roundAt(roundPrecision)(n))
+        val low = (quote \ "low")
+          .extract[List[Double]]
+          .map(n => roundAt(roundPrecision)(n))
+        val open = (quote \ "open")
+          .extract[List[Double]]
+          .map(n => roundAt(roundPrecision)(n))
+        val close = (quote \ "close")
+          .extract[List[Double]]
+          .map(n => roundAt(roundPrecision)(n))
         val volume = (quote \ "volume").extract[List[Long]]
         (timestamp.map(_.extract[Long]), high, low, open, close, volume)
       }
@@ -79,14 +125,8 @@ object EndpointDataLoader {
 
     // While it appears hacky, the nature of the JSON schema is deeply nested, so it must be flattened one more time
     val dataFrame = spark.createDataFrame(spark.sparkContext.parallelize(flatData.flatten), schema)
-
-    // TODO: logging
-    println("########################  We've made the dataFrame")
-    dataFrame.show(1)
-
     dataFrame
   }
-
 
   def fromV7Api(): DataFrame = {
     // Todo: remove duplicated code
@@ -126,23 +166,18 @@ object EndpointDataLoader {
     dataFrame
   }
 
-
-
   //noinspection AccessorLikeMethodIsUnit
   def toDatabase(dataFrame: DataFrame, table: String): Unit = {
 
-    println("########################  Attempting to write")
     dataFrame.write
-    .format("jdbc")
-    .option("url", dbUrl)
-    .option("dbtable", table)
-    .option("user", user)
-    .option("password", password)
-    .option("driver", driver)
-    .mode(mode)
-    .save()
-
-    println("@@@@@@@@@@@@@@@@@@@@ Write COMPLETED @@@@@@@@@@@@@@@@@@@@")
+      .format("jdbc")
+      .option("url", dbUrl)
+      .option("dbtable", table)
+      .option("user", user)
+      .option("password", password)
+      .option("driver", driver)
+      .mode(mode)
+      .save()
   }
 
   /* defined, but not used */
