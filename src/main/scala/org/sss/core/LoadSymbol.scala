58948c312a21c245b60106639fc6165090e22077
Michael Schmidt <michael.w.schmidt@proton.me>
2024-10-23 20:39:05 -0500

    Update integration debug and test in main()

--- src/main/scala/org/sss/core/LoadSymbol.scala
+++ src/main/scala/org/sss/core/LoadSymbol.scala
@@ -3,12 +3,15 @@ package org.sss.core
 
 object LoadSymbol {
 
-  private val table = System.getenv("SYMBOL")
+  private val sym = System.getenv("SYMBOL")
+  private val table = sym // System.getenv("TABLE")
 
   def main(args: Array[String]): Unit = {
 
-    val symbolDataFrame = fromV8API()
-    toDatabase(symbolDataFrame, table)
+    val loader = EndpointDataLoader(sym, 1722488400, 1725166800, "1d")
+    val df = loader.getSymbol
+
+    loader.loadSymbol(df, sym)
 
     println("we've reached the end")
   }
