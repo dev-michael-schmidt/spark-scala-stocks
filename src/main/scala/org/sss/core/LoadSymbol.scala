c32cae6672d663be4332d660571e6fa78637dece
Michael Schmidt <michael.w.schmidt@proton.me>
2024-10-27 23:21:35 -0500

    update driver code for debugging

--- src/main/scala/org/sss/core/LoadSymbol.scala
+++ src/main/scala/org/sss/core/LoadSymbol.scala
@@ -3,14 +3,21 @@ package org.sss.core
 
 object LoadSymbol {
 
-  private val sym = System.getenv("SYMBOL")
+  private val sym = "aapl" // System.getenv("SYMBOL")
   private val table = sym // System.getenv("TABLE")
 
   def main(args: Array[String]): Unit = {
 
-    val loader = new DataTransforms("aapl", 1, 2, "1d")
-    val df = loader.fetchData()
+    val apple = new DataPipeline()
+      .loadFromUrl(sym, 1719810000, 1722488400, "1d")
+      .getDataFrame
 
+    val appleWithTechnical = new TechnicalBuilder(apple)
+      .SMA(10)
+      .EMA(26)
+      .getDataFrame
+
+    appleWithTechnical.show(10)
 
     println("we've reached the end")
   }
