1046c3e4f99f56e2417764aee186e49e6297336f
Michael Schmidt <michael.w.schmidt@proton.me>
2024-11-04 22:23:54 -0600

    Move interpolate function

--- src/main/scala/org/sss/core/DataPipeline.scala
+++ src/main/scala/org/sss/core/DataPipeline.scala
@@ -86,51 +86,7 @@ class DataPipeline(private var dataFrame: DataFrame = null) extends DataPipeline
     this
   }
 
-  override def interpolate(): DataPipelineabstract = {
-
-    val windowSpec = Window.orderBy("tstamp")
-    val dataWithCurrPrev = this.dataFrame
-      .withColumn("prev_tstamp", lag("tstamp", 1).over(windowSpec))
-      .withColumn("prev_high", lag("high", 1).over(windowSpec))
-      .withColumn("prev_low", lag("low", 1).over(windowSpec))
-      .withColumn("prev_open", lag("open", 1).over(windowSpec))
-      .withColumn("prev_close", lag("close", 1).over(windowSpec))
-      .withColumn("prev_volume", lag("volume", 1).over(windowSpec))
-      .withColumn("diff", col("tstamp") - col("prev_tstamp"))
-
-    val contiguousDataframe = dataWithCurrPrev.flatMap(row => {
-      val tstamp = row.getAs[Long]("tstamp")
-      val prevTstamp = row.getAs[Long]("prev_tstamp")
-      val high = row.getAs[Double]("high")
-      val prevHigh = row.getAs[Double]("prev_high")
-      val low = row.getAs[Double]("low")
-      val prevLow = row.getAs[Double]("prev_low")
-      val open = row.getAs[Double]("open")
-      val prevOpen = row.getAs[Double]("prev_open")
-      val close = row.getAs[Double]("close")
-      val prevClose = row.getAs[Double]("prev_close")
-      val volume = row.getAs[Long]("volume")
-      val diff = row.getAs[Long]("diff")
-
-      // If the diff is greater than 86400 (1 day), split into two rows
-      if (diff > 86400) {
-        val interpolatedTstamp = prevTstamp + diff / 2
-        val interpolatedHigh = (high + prevHigh) / 2
-        val interpolatedLow = (low + prevLow) / 2
-        val interpolatedOpen = (open + prevOpen) / 2
-        val interpolatedClose = (close + prevClose) / 2
-
-        // Return both the interpolated row (with volume = 0) and the original row since.
-        // this has the effect of providing what is already present, and also adds the interpolated values
-        Seq(
-          (interpolatedTstamp, interpolatedHigh, interpolatedLow, interpolatedOpen, interpolatedClose, 0L), // Volume set to 0 for interpolated row
-          (tstamp, high, low, open, close, volume) // Original row
-        )
-      } else {
-        // Since there is no gap, return the original row
-        Seq((tstamp, high, low, open, close, volume))
-      }
-    }).toDF("tstamp", "high", "low", "open", "close", "volume")
+  override def writeToDatabase(table: String): Unit = {
 
     dataFrame = contiguousDataframe
       .drop("prev_tstamp")
