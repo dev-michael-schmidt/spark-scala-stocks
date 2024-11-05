d0d53e09e476fdbc8250d78d122e7363a677995e
Michael Schmidt <michael.w.schmidt@proton.me>
2024-11-04 22:27:53 -0600

    Update driver app

--- src/main/scala/org/sss/core/LoadSymbol.scala
+++ src/main/scala/org/sss/core/LoadSymbol.scala
@@ -1,23 +1,23 @@
 package org.sss.core
 
+import TechnicalImplicits.PipelineActions
 
 object LoadSymbol {
 
-  private val sym = "aapl" // System.getenv("SYMBOL")
-  private val table = sym // System.getenv("TABLE")
+  private val sym = System.getenv("SYMBOL")
+  private val table = sym
 
   def main(args: Array[String]): Unit = {
 
     val apple = new DataPipeline()
       .loadFromUrl(sym, 1719810000, 1722488400, "1d")
-      .getDataFrame
-
-    val appleWithTechnical = new TechnicalBuilder(apple)
+      .interpolate
+      .EMA(10)
       .SMA(10)
-      .EMA(26)
-      .getDataFrame
 
-    appleWithTechnical.show(10)
+    apple.writeToDatabase(sym)
+
+
 
     println("we've reached the end")
   }
